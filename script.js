18c30d5aa7dac86dd1633c7d4424b6c874838182
User <alicsebaiust01@gmail.com>
2024-04-14 03:36:06 +0600

    js work on progress

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // work on progress
+const todoList = document.getElementById("todoList");
+const newToDoInput = document.getElementById("newToDoInput");
+const addToDoBtn = document.getElementById("addToDoBtn");
+
+addToDoBtn.addEventListener("click",()=>{
+    const newToDoText = newToDoInput.value;
+    if(newToDoInput!=="") {
+        
+    }
+
+})
